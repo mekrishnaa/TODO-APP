eeb97474c0433b636b909a9cd2f5283cc3a9ec3f
krishna kumar <krishnakumar356808@gmail.com>
2022-03-02 19:11:24 +0530

    build added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import './styles/GlobalStyles.css'
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+// import '@fontsource/poppins';
+// import '@fontsource/poppins/500.css';
+// import '@fontsource/poppins/600.css';
+// import '@fontsource/poppins/700.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
